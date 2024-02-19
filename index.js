650fcf2f806484a65f32d166b8e2a82e4822e078
RowJMD <83046688+RowJMD@users.noreply.github.com>
2024-02-19 14:33:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ user.on('loggedOn', () => {
 var username2 = process.env.username2;
 var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
-
 var games2 = [730, 1446850, 1044450];  // Enter here AppIDs of the needed games
 var status2 = 1;  // 1 - online, 7 - invisible
-
 
 user2 = new steamUser();
 user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
@@ -63,4 +61,19 @@ user4.on('loggedOn', () => {
     if (user4.steamID != null) console.log(user4.steamID + ' - Inicio de sesión exitoso');
     user4.setPersona(status4);               
     user4.gamesPlayed(games4);
+});
+
+var username5 = process.env.username5;
+var password5 = process.env.password5;
+var shared_secret5 = process.env.shared5;
+
+var games5 = [730, 578080, 570];  // Ingresa aquí los AppIDs de los juegos necesarios
+var status5 = 1;  // 1 - en línea, 7 - invisible
+
+user5 = new steamUser();
+user5.logOn({"accountName": username5, "password": password5, "twoFactorCode": steamTotp.generateAuthCode(shared_secret5)});
+user5.on('loggedOn', () => {
+    if (user5.steamID != null) console.log(user5.steamID + ' - Inicio de sesión exitoso');
+    user5.setPersona(status5);               
+    user5.gamesPlayed(games5);
 });
